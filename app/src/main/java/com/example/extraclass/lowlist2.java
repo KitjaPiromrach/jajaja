7f2b7b4131b1463075a0f6bdfe5873a2b3960745
spellrhyme ex <spellrhyme ex>
2016-11-05 11:37:27 +0700

    jajaja

---
+++ app/src/main/java/com/example/extraclass/lowlist2.java
@@ -0,0 +1,13 @@
+package com.example.extraclass;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class lowlist2 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lowlist2);
+    }
+}
---
+++ app/src/main/java/com/example/extraclass/register.java
@@ -0,0 +1,13 @@
+package com.example.extraclass;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
